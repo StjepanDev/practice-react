9c4ac13af6ae2785c56866672f2f7eddd9173e90
StjepanDev <stjepansuhic@yahoo.com>
2023-02-07 00:41:48 +0100

    icons

--- src/projects/03-like-my-photo/LikePhotoApp.jsx
+++ src/projects/03-like-my-photo/LikePhotoApp.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import Title from "../components/Title";
+import {
+  BsFillEmojiSmileFill,
+  BsFillSuitHeartFill,
+  BsSuitHeart,
+} from "react-icons/bs";
+import { AiOutlineComment } from "react-icons/ai";
+import Doggy from "./img/images.jpg";
 
 export default function LikePhotoApp() {
+  let like = true;
   return (
     <div className='container text-center'>
       <Title text={"Like My Photo"} />
@@ -11,13 +19,21 @@ export default function LikePhotoApp() {
         style={{ width: 300, cursor: "pointer" }}
       >
         <div className='card-header fs-xl'>
+          <BsFillEmojiSmileFill className='mr-2' />
           <small>Doggy</small>
         </div>
-        <img src='' alt='' style={{ height: "fit-content" }} />
+        <img src={Doggy} alt='' style={{ height: "fit-content" }} />
         <div
           className='card-footer fs-xl d-flex'
           style={{ justifyContent: "space-between" }}
-        ></div>
+        >
+          <AiOutlineComment />{" "}
+          {like ? (
+            <BsFillSuitHeartFill className='text-danger' />
+          ) : (
+            <BsSuitHeart />
+          )}
+        </div>
       </div>
     </div>
   );
